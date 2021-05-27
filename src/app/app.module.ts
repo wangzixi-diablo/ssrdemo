076f582ad691a5b65b3276abd8122751a900237f
wangzixi-diablo <cle.ee@163.com>
2021-05-27 16:27:35 +0800

    first upload after add using schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
-
+import { BrowserModule, BrowserTransferStateModule } from '@angular/platform-browser';
+import { EffectsModule } from "@ngrx/effects";
+import { StoreModule } from "@ngrx/store";
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpartacusModule } from './spartacus/spartacus.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    SpartacusModule,
+    BrowserTransferStateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
